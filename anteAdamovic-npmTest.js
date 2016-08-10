569b5a281150ffb49865568a8ee2893cbd44d89e
anteks <anteks@keepersolutions.com>
2016-08-10 16:05:02 +0200

    Added support for npm and regular include with script tags

--- anteAdamovic-npmTest.js
+++ anteAdamovic-npmTest.js
@@ -1,3 +1,14 @@
-exports.anteAdamovicNpmTest = function(){
-  return "anteAdamovic-npmTest";
-}
+(function (root, factory) {
+  if(typeof module === "object" && module.exports){
+    module.exports = factory(require("anteadamovicnpmtest"));
+  }
+  else{
+    root.anteAdamovicNpmTest = factory("anteadamovicnpmtest");
+  }
+}(this, function(){
+  return {
+    test: function(){
+      return "anteAdamovic-testFunction";
+    }
+  }
+}));
